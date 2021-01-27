fede6d7ccdb34ae3208a18f4ef8de3ec2877ef9c
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-01-28 00:51:47 +0200

    Task-2 ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Header from './components/Header/Header';
+import Layout from './components/Layout/Layout';
+import Footer from './components/Footer/Footer';
+import bg1 from './images/bg1.jpg';
+import bg2 from './images/bg2.jpg';
+// console.log(bg1);
+// const bg1 = '../../images/bg1.jpg'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header title={'This is title'} descr={'This is Description!'} />
+      <Layout
+        id={'firstLayout'}
+        urlBg={bg1}
+        //  style = {{backgroundImage: "url(" +{bg1} + ")"  }}
+        //  backgroundImage: "url(" + { Background } + ")"
+      />
+      <Layout id={'SecondLayout'} colorBg="purple" />
+      <Layout id={'ThirdLayout'} urlBg={bg2} />
+      <Footer />
+    </>
   );
 }
 
